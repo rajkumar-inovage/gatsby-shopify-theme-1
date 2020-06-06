dad535f04fd36a4f1c7ab8211becc14b405030fe
Ankit Shukla <4nkit5hukla@gmail.com>
2020-06-06 19:13:47 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/collectionsPage/index.js
+++ src/templates/collectionsPage/index.js
@@ -8,38 +8,8 @@ import StoreContext from "~/context/store";
 import GridIcon from "~/components/grid-icon";
 import ListIcon from "~/components/list-icon";
 const CollectionsPage = ({ data }) => {
-
-
   const context = useContext(StoreContext);
-  const [sort, setSort] = useState(context.filteredSort)
-
-  useEffect(() => {
-    context.updateFilterSort(sort)
-  }, [context, sort])
-
-  const sorts = []
-
-  sorts.push(
-    <>
-      <option key={0} value="featured">
-        Featured
-      </option>
-      <option key={1} value="A-Z">
-        Alphabetically, A-Z
-      </option>
-      <option key={2} value="Z-A">
-        Alphabetically, Z-A
-      </option>
-      <option key={3} value="low">
-        Price, low to high
-      </option>
-      <option key={4} value="high">
-        Price, high to low
-      </option>
-    </>
-  )
-
-
+  const { checkout } = context;
   const MAX_LENGTH = 100;
   const [gridClass, setGridClass] = useState("col-md-4");
   const [imgClass, setImgClass] = useState("col-md-12");
@@ -48,15 +18,15 @@ const CollectionsPage = ({ data }) => {
   const [descClass, setDescClass] = useState("d-none");
   const [rowpriceClass, setRowpriceClass] = useState("d-inline-block");
   const [addcartClass, setAddcartClass] = useState("d-block");
-  
-  const { checkout } = context;
+  const [sort, setSort] = useState("featured");
+  const products = data.shopifyCollection.products;
   const buttonClasses =
     "btn-link bg-transparent border-0 text-decoration-none text-body p-0";
-  const getPrice = price =>
+  const getPrice = (price) =>
     Intl.NumberFormat(undefined, {
       currency: checkout.currencyCode ? checkout.currencyCode : "CAD",
       minimumFractionDigits: 2,
-      style: "currency"
+      style: "currency",
     }).format(parseFloat(price ? price : 0));
   const toggleGrid = (event, size) => {
     event.preventDefault();
@@ -96,6 +66,34 @@ const CollectionsPage = ({ data }) => {
       setRowpriceClass("d-inline-block");
       setAddcartClass("d-block");
     }
+  };
+  useEffect(() => {
+    //context.updateFilterSort(sort);
+  }, [context, sort]);
+  const sorts = [
+    {
+      value: "featured",
+      title: "Featured",
+    },
+    {
+      value: "A-Z",
+      title: "Alphabetically, A-Z",
+    },
+    {
+      value: "Z-A",
+      title: "Alphabetically, Z-A",
+    },
+    {
+      value: "low",
+      title: "Price, low to high",
+    },
+    {
+      value: "high",
+      title: "Price, high to low",
+    }
+  ];
+  const reSort = (e) => {
+    setSort(e.target.value);
   };
   return (
     <>
@@ -145,7 +143,7 @@ const CollectionsPage = ({ data }) => {
                   style={{
                     fontSize: "2.5rem",
                     color: "#000",
-                    lineHeight: "24px"
+                    lineHeight: "24px",
                   }}
                 >
                   {data.shopifyCollection.title}
@@ -159,25 +157,28 @@ const CollectionsPage = ({ data }) => {
         <Container>
           <Row className="mx-0">
             <Col className="col-6 d-none d-lg-block">
-              <button onClick={e => toggleGrid(e, 1)} className={buttonClasses}>
+              <button
+                onClick={(e) => toggleGrid(e, 1)}
+                className={buttonClasses}
+              >
                 <ListIcon />
               </button>
               <button
-                onClick={e => toggleGrid(e, 2)}
+                onClick={(e) => toggleGrid(e, 2)}
                 className={buttonClasses + " ml-3"}
               >
                 <GridIcon />
                 <sup className="ml-1">2</sup>
               </button>
               <button
-                onClick={e => toggleGrid(e, 3)}
+                onClick={(e) => toggleGrid(e, 3)}
                 className={buttonClasses + " ml-3"}
               >
                 <GridIcon />
                 <sup className="ml-1">3</sup>
               </button>
               <button
-                onClick={e => toggleGrid(e, 4)}
+                onClick={(e) => toggleGrid(e, 4)}
                 className={buttonClasses + " ml-3"}
               >
                 <GridIcon />
@@ -185,133 +186,153 @@ const CollectionsPage = ({ data }) => {
               </button>
             </Col>
             <Col className="col-6 text-right">
-            <label htmlFor="sortBy" className="has-text-weight-semibold is-uppercase" style={{ margin: "-20px" }}>SORT BY :
-                  <div className="field">
-                    <div className="control">
-                      <div className="select">
-                        <select
-                          defaultValue={sort}
-                          onBlur={e => setSort(e.target.value)}
-                          id="sortBy"
-                        >
-                          {sorts}
-                        </select>
-                      </div>
+              <label
+                htmlFor="sortBy"
+                className="has-text-weight-semibold is-uppercase"
+                style={{ margin: "-20px" }}
+              >
+                SORT BY :
+                <div className="field">
+                  <div className="control">
+                    <div className="select">
+                      <select
+                        defaultValue={sort}
+                        onBlur={(e) => reSort(e)}
+                        id="sortBy"
+                      >
+                        {sorts.map(({ value, title }, index) => (
+                          <option key={index} value={value}>
+                            {title}
+                          </option>
+                        ))}
+                      </select>
                     </div>
                   </div>
-                </label>
+                </div>
+              </label>
             </Col>
           </Row>
           <Row className="mt-3 mt-lg-5 product-layout">
+            {products.length ? (
+              products
+                .sort(
+                  sort === "low"
+                    ? (a, b) => a.variants[0].price - b.variants[0].price
+                    : sort === "high"
+                    ? (a, b) => b.variants[0].price - a.variants[0].price
+                    : sort === "Z-A"
+                    ? (a, b) => b.title.localeCompare(a.title)
+                    : (a, b) => a.title.localeCompare(b.title)
+                )
+                .map(
+                  ({ title, handle, description, images, variants }, index) => (
+                    <div key={index} className={gridClass + " mb-3 mb-lg-5"}>
+                      <div className={displayClass + " trending-products"}>
+                        <div className={imgClass}>
+                          <Link to={`/product/${handle}/`} className="">
+                            <div className="tp-image">
+                              {images.length &&
+                                images[0].localFile.childImageSharp.original
+                                  .src && (
+                                  <img
+                                    src={
+                                      images[0].localFile.childImageSharp
+                                        .original.src
+                                    }
+                                    alt={images[0].altText}
+                                  />
+                                )}
 
-                
-
-            {data.shopifyCollection.products ? (
-              data.shopifyCollection.products.map(
-                ({ id, title, handle, description, images, priceRange }) => (
-                  <div className={gridClass + " mb-3 mb-lg-5"}>
-                    <div className={displayClass + " trending-products"}>
-                      <div className={imgClass}>
-                        <Link to={`/product/${handle}/`} className="">
-                          <div className="tp-image">
-                            {images[0] &&
-                              images[0].localFile.childImageSharp.fluid.src && (
-                                <img
-                                  src={
-                                    images[0].localFile.childImageSharp.fluid
-                                      .src
+                              <div className="add-to-cart d-inline w-auto p-0">
+                                <button
+                                  className={
+                                    addcartClass +
+                                    " josefin-sans-b cart-btn border border-dark btns position-relative"
                                   }
-                                  alt=""
-                                />
-                              )}
-
-                            <div className="add-to-cart d-inline w-auto p-0">
-                              <button
-                                className={addcartClass + " josefin-sans-b cart-btn border border-dark btns position-relative"}
-                                style={{ fontSize: "0.8rem" }}
-                              >
-                                Add To Cart
-                              </button>
+                                  style={{ fontSize: "0.8rem" }}
+                                >
+                                  Add To Cart
+                                </button>
+                              </div>
                             </div>
-                          </div>
-                        </Link>
-                      </div>
-                      <div className={cntClass}>
-                        <div className="tp-details">
-                          <div className="review-and-price d-block mt-3">
-                            <span className="star-value d-none w-50 pl-2 pl-lg-3">
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                            </span>
-                            <span
-                              className={
-                                rowpriceClass +
-                                " price josefin-sans-sb text-right w-100 pr-2 pr-lg-3"
-                              }
-                              style={{ fontSize: "1.2rem", color: "#000" }}
-                            >
-                              {getPrice(priceRange.maxVariantPrice.amount)}
-                            </span>
-                          </div>
-
-                          <Link
-                            to={`/product/${handle}/`}
-                            className="josefin-sans-b mt-2 d-block"
-                            style={{ textDecoration: "none" }}
-                          >
-                            <h3
-                              className=""
-                              style={{ fontSize: "1.8rem", color: "#000" }}
-                            >
-                              {title}
-                            </h3>
                           </Link>
-                          <div className={descClass}>
-                            <span
-                              className="price josefin-sans-b d-inline-block text-left w-50 pr-2 pr-lg-3"
-                              style={{ fontSize: "1.2rem", color: "#000" }}
-                            >
-                              {getPrice(priceRange.maxVariantPrice.amount)}
-                            </span>
-                          </div>
-                          <div className={descClass + " p-desc"}>
-                            <div className="desc-p col-12 col-lg-9 px-0">
-                              <p
-                                className="josefin-sans"
-                                style={{ fontSize: "1.2rem" }}
+                        </div>
+                        <div className={cntClass}>
+                          <div className="tp-details">
+                            <div className="review-and-price d-block mt-3">
+                              <span className="star-value d-none w-50 pl-2 pl-lg-3">
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                              </span>
+                              <span
+                                className={
+                                  rowpriceClass +
+                                  " price josefin-sans-sb text-right w-100 pr-2 pr-lg-3"
+                                }
+                                style={{ fontSize: "1.2rem", color: "#000" }}
                               >
-                                {description.substring(0, MAX_LENGTH)}...
-                              </p>
-                              <Link
-                                to={`/product/${handle}/`}
-                                className="more josefin-sans-sb text-decoration-none position-relative"
-                                style={{ fontSize: "1.3rem", color: "#000" }}
-                              >
-                                More
-                              </Link>
+                                {getPrice(variants[0].price)}
+                              </span>
                             </div>
-                            <div
-                              className="atc text-center col-12 col-lg-3 px-0"
-                              style={{ minWidth: "150px" }}
+
+                            <Link
+                              to={`/product/${handle}/`}
+                              className="josefin-sans-b mt-2 d-block"
+                              style={{ textDecoration: "none" }}
                             >
-                              <Link
-                                to={`/product/${handle}/`}
-                                className="text-decoration-none px-3 py-3 d-block josefin-sans-b cart-btn border border-dark btns position-relative"
-                                style={{ fontSize: "0.8rem" }}
+                              <h3
+                                className=""
+                                style={{ fontSize: "1.8rem", color: "#000" }}
                               >
-                                ADD TO CART
-                              </Link>
+                                {title}
+                              </h3>
+                            </Link>
+                            <div className={descClass}>
+                              <span
+                                className="price josefin-sans-b d-inline-block text-left w-50 pr-2 pr-lg-3"
+                                style={{ fontSize: "1.2rem", color: "#000" }}
+                              >
+                                {getPrice(variants[0].price)}
+                              </span>
+                            </div>
+                            <div className={descClass + " p-desc"}>
+                              <div className="desc-p col-12 col-lg-9 px-0">
+                                <p
+                                  className="josefin-sans"
+                                  style={{ fontSize: "1.2rem" }}
+                                >
+                                  {description.substring(0, MAX_LENGTH)}...
+                                </p>
+                                <Link
+                                  to={`/product/${handle}/`}
+                                  className="more josefin-sans-sb text-decoration-none position-relative"
+                                  style={{ fontSize: "1.3rem", color: "#000" }}
+                                >
+                                  More
+                                </Link>
+                              </div>
+                              <div
+                                className="atc text-center col-12 col-lg-3 px-0"
+                                style={{ minWidth: "150px" }}
+                              >
+                                <Link
+                                  to={`/product/${handle}/`}
+                                  className="text-decoration-none px-3 py-3 d-block josefin-sans-b cart-btn border border-dark btns position-relative"
+                                  style={{ fontSize: "0.8rem" }}
+                                >
+                                  ADD TO CART
+                                </Link>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  )
                 )
-              )
             ) : (
               <p>No Products found!</p>
             )}
@@ -330,12 +351,6 @@ export const query = graphql`
         handle
         title
         description
-        priceRange {
-          maxVariantPrice {
-            amount
-            currencyCode
-          }
-        }
         images {
           altText
           localFile {
@@ -343,7 +358,21 @@ export const query = graphql`
               fluid {
                 src
               }
+              original {
+                width
+                src
+                height
+              }
             }
+          }
+        }
+        variants {
+          price
+        }
+        priceRange {
+          maxVariantPrice {
+            amount
+            currencyCode
           }
         }
       }
